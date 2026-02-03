26fdd312a9d37a1a01118914c2f68324c0ce24e6
waitingforrply-hub <waiting.for.rply@gmail.com>
2026-02-03 23:15:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // ---------- Stage 3: Photos ----------
 const photos = [
   { src: "images/1.jpg.JPG", cap: "Exhibit A: Mid way through 💖" },
-  { src: "images/2.jpg.JPG", cap: "Exhibit B: Manifestation in background 😌🫶" },
+  { src: "images/2.jpg.JPG", cap: "Exhibit B: Manifestation in background 😌" },
   { src: "images/3.jpg.JPG", cap: "Exhibit C: You know why I like this 📸" },
   { src: "images/4.jpg.JPG", cap: "Exhibit D: Where it all started ✨" }
 ];
