5f4b8aeb4ccc12f73af9d599d3834cbfc214483c
waitingforrply-hub <waiting.for.rply@gmail.com>
2026-02-03 22:26:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -220,7 +220,7 @@ window.addEventListener("DOMContentLoaded", () => {
   { src: "images/1.jpg", cap: "Exhibit A: Mid way through 💘" },
   { src: "images/2.jpg", cap: "Exhibit B: Manifestation in background 😮‍💨" },
   { src: "images/3.jpg", cap: "Exhibit C: You know why I like this 🫶" }
-  { src: "images/4.jpg", cap: "Exhibit D: Where it all started. 🫶" }
+  { src: "images/4.jpg", cap: "Exhibit D: Where it all started 🫶" }
 ];
 
   let p = 0;
